1a7a983e6a5197f49be3a0da116cb16e4f2af711
Bryan Changala <bryan.changala@gmail.com>
2020-12-03 09:19:05 -0500

    added loadpes for PES library

--- nitrogen/pes/library/__init__.py
+++ nitrogen/pes/library/__init__.py
@@ -2,5 +2,14 @@
 nitrogen.pes.library
 --------------------
 
+A collection of potential energy surfaces. These should
+be accessed via the :func:`nitrogen.pes.loadpes()` function.
+
+PES name        Description
+==============  ==================================================
+H2O_PJT1996     Water. Cartesian coordinates H--O--H.
+==============  ==================================================
+
+
 """
 
