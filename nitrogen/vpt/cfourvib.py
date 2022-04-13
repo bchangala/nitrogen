a1955e924c25077e82bdf927b6b53f101ea58420
Bryan Changala <bryan.changala@gmail.com>
2022-04-13 15:52:06 -0400

    added cfour cubic force constants file parser

--- nitrogen/vpt/cfourvib.py
+++ nitrogen/vpt/cfourvib.py
@@ -8,6 +8,7 @@ interface routines.
 
 import numpy as np 
 import nitrogen.constants
+import nitrogen.autodiff.forward as adf 
 
 __all__ = ['read_QUADRATURE']
 
@@ -98,3 +99,99 @@ def read_QUADRATURE(filename, use_bohr = False):
         ref_geo *= nitrogen.constants.a0
         
     return freq, T, ref_geo 
+
+def read_cubic(filename, nvib = None, offset = 7):
+    """
+    Read a CFOUR cubic force constants text file
+    
+    Parameters
+    ----------
+    filename : str
+        The file path
+    nvib : int, optional
+        The number of vibrational modes. If None, the total
+        will be inferred from the input file.
+    offset : int, optional
+        The mode numbering offset from zero. The default is 7,
+        which is usually what CFOUR files require (6 for 
+        rot-trans modes and 1 for zero-indexing).
+    
+    Returns
+    -------
+    F : ndarray
+        The scaled derivative including up to cubic derivatives.
+        (The zeroth, first, and second derivatives are zeroed.)
+    
+    Notes
+    -----
+    The deriative array is returned in standard scaled format, i.e.
+    the derivatives are divided by the factorial of their
+    respective multi-index.
+    
+    """
+    
+    with open(filename,'r') as file:
+        lines = file.readlines()
+
+    # Figure out maximum index value 
+    if nvib is None:
+        maxindex = 0 
+        for line in lines :
+            indices = [int(x) for x in line.split()[0:3]]
+            maxindex = max(maxindex, max(indices))
+        nvib = maxindex - offset + 1 
+
+
+    # Create a derivative array up to cubics 
+    nd = adf.nderiv(3, nvib) 
+    F = np.zeros((nd,)) 
+    midx = np.zeros((nvib,), dtype = np.uint32) 
+
+    # Calculate a binomial table 
+    nck = adf.ncktab(nvib + 2, min(nvib, 2)) 
+
+    for line in lines:
+        
+        if not line:
+            continue # skip any empty lines 
+            
+        toks = line.split()
+        if len(toks) != 4:
+            raise ValueError('4 entries expected on each line!')            
+        
+        # Grab the indices and apply offset
+        # Sort the modes in ascending order
+        modes = [int(x) - offset for x in toks[0:3]]
+        modes.sort() 
+        
+        # Determine the position in the derivative array
+        # of this multi-index `midx`
+        midx.fill(0) 
+        for i in range(3):
+            midx[modes[i]] += 1
+        pos = adf.idxpos(midx, nck) 
+        
+        # Grab the force constant 
+        coeff = float(toks[-1])
+        
+        # Determine what permutation factor needs to
+        # be applied before storing the force
+        # constant in the derivative array
+        # (`modes` is in ascending sorted order)
+        #
+        if modes[0] == modes[1] and modes[1] == modes[2]:
+            # iii type
+            #
+            F[pos] = coeff / 6.0  # 1/3!
+        elif modes[0] == modes[1] or modes[1] == modes[2]:
+            # iij or ijj type 
+            F[pos] = coeff / 2.0  # 1/2! * 1/1!
+        else: 
+            # ijk type 
+            F[pos] = coeff 
+        
+    # F now contains all elements 
+    
+    return F 
+    
+    
