e63e250b703030a8d5ad2f06919ae62874b48ee4
Bryan Changala <bryan.changala@gmail.com>
2024-08-03 11:21:20 -0400

    docs, python version

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setuptools.setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	python_requires = '>=3.6', 
+	python_requires = '>=3.7.11', 
     install_requires=install_requires,
     cmdclass = cmdclass,
     ext_modules = ext_modules
